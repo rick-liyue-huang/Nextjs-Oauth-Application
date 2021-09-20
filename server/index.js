87e0a063e385b81cca5fb2ef170c9f075dc15050
rickhuang <rick.liyue.huang@gmail.com>
2021-09-20 15:32:13 +1000

    import antd and style

---
+++ server/index.js
@@ -0,0 +1,53 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const next = require('next');
+
+// deal with the development environment
+const dev = process.env.NODE_ENV !== 'production';
+// create Next App
+const app = next({dev});
+
+const handle = app.getRequestHandler();
+
+// Next app firstly load all the pages and then request other needs
+app.prepare()
+  .then(() => {
+  const server = new Koa();
+  const router = new Router();
+
+  server.use(router.routes());
+
+  // middlewares
+  server.use(async (ctx, next) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+    server.listen(3001, () => {
+      console.log('koa server listening on 3001');
+    });
+});
+
+
+
+
+
+/*router.get('/test/:id', (ctx) => {
+    const path = ctx.path;
+    const method = ctx.method;
+    const id = ctx.params.id;
+    // ctx.body = `<p>request test - ${path} - ${method} - ${id}</p>`
+    ctx.body = {
+      success: true,
+      path,
+      method,
+      id
+    };
+    ctx.set('Content-Type', 'application/json');
+  });
+
+  server.use(async (ctx, next) => {
+    ctx.body = `<span>koa Render, </span>`;
+    next();
+  });
+   */
+
